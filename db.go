7fa7ff596200d6e68553cbd664183a13a540dae0
pchchv <ipchchv@gmail.com>
2022-09-26 14:20:53 +0300

    Implement a load db

--- db.go
+++ db.go
@@ -1,9 +1,32 @@
 package main
 
-// db is the file where the game statistics are stored.
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/pkg/errors"
+)
+
+// File where the game statistics are stored
 type db struct {
 	// Guesses stores the win statistics of each game. Guesses[0] is the number
 	// of games that were lost, Guesses[1] is the number of games that were won
 	// with 1 guess, etc.
 	Guesses [numGuesses + 1]int `json:"guesses"`
+}
+
+// Reads the database from dbPath
+func loadDb() (*db, error) {
+	file, err := os.Open(pathDb)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return &db{}, nil
+		}
+		return nil, errors.Wrap(err, "could not find database")
+	}
+	var db db
+	if err := json.NewDecoder(file).Decode(&db); err != nil {
+		return nil, errors.Wrap(err, "could not read from database")
+	}
+	return &db, nil
 }
