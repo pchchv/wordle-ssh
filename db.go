cad74bd8962681e59730868385fd7741c7f0545d
pchchv <ipchchv@gmail.com>
2022-09-26 14:22:35 +0300

    Implement a score func for db

--- db.go
+++ db.go
@@ -11,7 +11,7 @@ import (
 type db struct {
 	// Guesses stores the win statistics of each game. Guesses[0] is the number
 	// of games that were lost, Guesses[1] is the number of games that were won
-	// with 1 guess, etc.
+	// with 1 guess, etc
 	Guesses [numGuesses + 1]int `json:"guesses"`
 }
 
@@ -29,4 +29,15 @@ func loadDb() (*db, error) {
 		return nil, errors.Wrap(err, "could not read from database")
 	}
 	return &db, nil
+}
+
+// Returns the current total score based on the statistics
+// A game that was lost provides 0 points. A game that was won provides 50 points,
+// plus an extra 10 points for each remaining guess
+func (db *db) score() int {
+	score := 0
+	for i, guess := range db.Guesses[1:] {
+		score += (100 - 10*i) * guess
+	}
+	return score
 }
